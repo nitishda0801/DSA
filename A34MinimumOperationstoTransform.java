9140bc5d1d1bd1390df325adb6ba93fcaed9e943
NITISH KUMAR <98886280+nitishda0801@users.noreply.github.com>
2023-06-30 10:38:07 +0530

    Update and rename A34 Minimum Operations to Transform to A34MinimumOperationstoTransform.java

--- A34MinimumOperationstoTransform.java
+++ A34MinimumOperationstoTransform.java
@@ -1,3 +1,4 @@
+/*
 Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
 
 You have the following three operations permitted on a word:
@@ -30,7 +31,10 @@ qntjmvock
 fvbd
 Sample Output 1
 
+
 8
+
+*/
 import java.util.Scanner;
 
 public class Solution {
